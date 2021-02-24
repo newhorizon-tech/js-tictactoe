2e5fbdebee357d02098d6d0989d4b8c7e32cf2e8
newhorizon-tech <alaukikinbox@gmail.com>
2021-02-24 16:13:32 +0530

    Set up webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.scss';
+
+console.log("hmm");
+
+let i = 5+2;
+
+console.log(i);
+
+console.log("Webpack serve works!")
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    inline: false,
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [{
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+
+    ],
+  }
+};
